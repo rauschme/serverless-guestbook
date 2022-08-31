e7f64b9d7aa16f3cefeed1af8e391a06168924ce
rauschme <98319434+rauschme@users.noreply.github.com>
2022-08-31 07:01:54 +0200

    Update guestbook.js
    update apiUrl  with APIUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://3400ba70.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
